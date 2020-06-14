3c5cbb269eeddc00e4c2d45066ca57cd40e406c2
Phúc Tài - Group Tân <phuctai.nina@gmail.com>
2020-06-14 09:49:02 +0700

    Nina PDO

--- admin/ajax/ajax_attribute.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include "ajax_config.php";
-
-	$id = htmlspecialchars($_POST["id"]);
-
-	if($id) $row = $d->rawQuery("delete from #_attribute where id = ?",array($id));
-?>
--- sources/seo.php
+++
@@ -1,7 +0,0 @@
-<ul class="h-card hidden">
-    <li class="h-fn fn"><?=$setting['ten'.$lang]?></li>
-    <li class="h-org org"><?=$setting['ten'.$lang]?></li>
-    <li class="h-tel tel"><?=preg_replace('/[^0-9]/','',$setting['hotline']);?></li>
-    <li><a class="u-url ul" href="<?=$config_base?>"><?=$config_base?></a></li>
-</ul>
-<h1 class="hidden-seoh"><?=$seo_h1?></h1>
