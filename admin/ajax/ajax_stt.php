73d47bf3f82beda1be2513ce667b34b1eecafa89
Phúc Tài - Group Tân <phuctai.nina@gmail.com>
2020-06-24 20:16:20 +0700

    Nina PDO

---
+++ admin/ajax/ajax_stt.php
@@ -0,0 +1,12 @@
+<?php
+	include "ajax_config.php";
+
+	$table = htmlspecialchars($_POST['table']);
+	$id = htmlspecialchars($_POST['id']);
+	$value = htmlspecialchars($_POST['value']);
+
+	$data['stt'] = $value;
+	
+	$d->where('id',$id);
+	$d->update($table,$data);
+?>
