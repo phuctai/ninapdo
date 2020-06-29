657aa05dc39d17a2e6b77542d7d6c28d8b3bbcb8
Phúc Tài <phuctai.nina@gmail.com>
2020-06-29 08:22:30 +0700

    Nina PDO

---
+++ templates/layout/slide.php
@@ -0,0 +1,26 @@
+<?php if(count($slider)) { ?>
+    <div class="slideshow">
+        <div class="box-slide">
+            <div class="slider-wrapper theme-default">
+                <div id="slider-main" class="nivoSlider">
+                    <?php for($i=0;$i<count($slider);$i++) { ?>
+                        <?php $captionSlide = ($slider[$i]['ten'.$lang]!='' || $slider[$i]['mota'.$lang]!='') ? "title='#slide-caption".$i."'" : "" ; ?>
+                        <a href="<?=$slider[$i]['link']?>" title="<?=$slider[$i]['ten'.$lang]?>"><img src="<?=THUMBS?>/1366x600x1/<?=UPLOAD_PHOTO_L.$slider[$i]['photo']?>" alt="<?=$slider[$i]['ten'.$lang]?>" <?=$captionSlide?>/></a>
+                    <?php } ?>
+                </div>
+                <?php /* for($i=0;$i<count($slider);$i++) { if($slider[$i]['ten'.$lang] || $slider[$i]['mota'.$lang]) { ?>
+                    <div id="slide-caption<?=$i?>" class="nivo-html-caption">
+                        <a class="grid-caption" href="<?=$slider[$i]['link']?>">
+                            <div class="box-caption">
+                                <div class="inner-caption">
+                                    <p class="label-caption"><?=$slider[$i]['ten'.$lang]?></p>
+                                    <p class="info-caption"><?=$slider[$i]['mota'.$lang]?></p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                <?php } } */ ?>
+            </div>
+        </div>
+    </div>
+<?php } ?>
