82b6fe433c7ff62d2b2702dbb598226d9a596cd2
Phúc Tài <phuctai.nina@gmail.com>
2020-07-06 01:54:07 +0000

    Nina PDO

--- sources/js.php
+++ sources/js.php
@@ -1,6 +1,7 @@
 <!-- Var Js -->
 <script type="text/javascript">
     var CONFIG_BASE = '<?=$config_base?>';
+    var NN_MSHD = '<?=NN_MSHD?>';
     var WEBSITE_NAME = '<?=($setting['ten'.$lang]) ? $setting['ten'.$lang] : $setting['title'.$seolang]?>';
     var DEBUG_JS = <?=($config['website']['debug-js']) ? 'true' : 'false'?>;
     var TIMENOW = '<?=date("Y/m/d",time())?>';
@@ -23,22 +24,22 @@
 <script src="assets/basket/basket.full.js"></script>
 <script type="text/javascript">
     basket.require(
-        { url: 'assets/bootstrap/js/bootstrap.js', key: 'bootstrap', skipCache: DEBUG_JS },
-        { url: 'assets/js/wow.min.js', key: 'wow', skipCache: DEBUG_JS },
-        { url: 'assets/mmenu/mmenu.js', key: 'mmenu', skipCache: DEBUG_JS },
-        { url: 'assets/nivoslider/jquery.nivo.slider.js', key: 'nivo', skipCache: DEBUG_JS },
-        { url: 'assets/simplyscroll/jquery.simplyscroll.js', key: 'simplyscroll', skipCache: DEBUG_JS },
-        { url: 'assets/owlcarousel2/owl.carousel.js', key: 'owl', skipCache: DEBUG_JS },
-        { url: 'assets/magiczoomplus/magiczoomplus.js', key: 'magiczoomplus', skipCache: DEBUG_JS },
-        { url: 'assets/slick/slick.js', key: 'slick', skipCache: DEBUG_JS },
-        { url: 'assets/fancybox3/jquery.fancybox.js', key: 'fancybox', skipCache: DEBUG_JS },
-        { url: 'assets/photobox/photobox.js', key: 'photobox', skipCache: DEBUG_JS },
-        { url: 'assets/datetimepicker/php-date-formatter.min.js', key: 'formatter', skipCache: DEBUG_JS },
-        { url: 'assets/datetimepicker/jquery.mousewheel.js', key: 'mousewheel', skipCache: DEBUG_JS },
-        { url: 'assets/datetimepicker/jquery.datetimepicker.js', key: 'datetimepicker', skipCache: DEBUG_JS },
-        { url: 'assets/toc/toc.js', key: 'toc', skipCache: DEBUG_JS },
-        { url: 'assets/js/functions.js', key: 'functions', skipCache: DEBUG_JS },
-        { url: 'assets/js/apps.js', key: 'apps', skipCache: DEBUG_JS }
+        { url: 'assets/bootstrap/js/bootstrap.js', key: 'bootstrap-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/js/wow.min.js', key: 'wow-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/mmenu/mmenu.js', key: 'mmenu-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/nivoslider/jquery.nivo.slider.js', key: 'nivo-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/simplyscroll/jquery.simplyscroll.js', key: 'simplyscroll-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/owlcarousel2/owl.carousel.js', key: 'owl-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/magiczoomplus/magiczoomplus.js', key: 'magiczoomplus-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/slick/slick.js', key: 'slick-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/fancybox3/jquery.fancybox.js', key: 'fancybox-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/photobox/photobox.js', key: 'photobox-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/datetimepicker/php-date-formatter.min.js', key: 'formatter-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/datetimepicker/jquery.mousewheel.js', key: 'mousewheel-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/datetimepicker/jquery.datetimepicker.js', key: 'datetimepicker-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/toc/toc.js', key: 'toc-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/js/functions.js', key: 'functions-'+NN_MSHD, skipCache: DEBUG_JS },
+        { url: 'assets/js/apps.js', key: 'apps-'+NN_MSHD, skipCache: DEBUG_JS }
     );
     if(DEBUG_JS) basket.clear();
 </script>
