46ba9ce598e081f89a1a21cace9100cff5e99b27
Phúc Tài - Group Tân <phuctai.nina@gmail.com>
2020-07-18 21:23:33 +0700

    Nina PDO

---
+++ admin/templates/static/man/item_add_tpl.php
@@ -0,0 +1,149 @@
+<?php
+    $linkSave = "index.php?com=static&act=save&type=".$type;
+?>
+<!-- Content Header -->
+<section class="content-header text-sm">
+    <div class="container-fluid">
+        <div class="row">
+            <ol class="breadcrumb float-sm-left">
+                <li class="breadcrumb-item"><a href="index.php" title="Bảng điều khiển">Bảng điều khiển</a></li>
+                <li class="breadcrumb-item active">Quản lý trang tĩnh</li>
+            </ol>
+        </div>
+    </div>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <form class="validation-form" novalidate method="post" action="<?=$linkSave?>" enctype="multipart/form-data">
+        <div class="card-footer text-sm sticky-top">
+            <button type="submit" class="btn btn-sm bg-gradient-primary submit-check"><i class="far fa-save mr-2"></i>Lưu</button>
+            <button type="reset" class="btn btn-sm bg-gradient-secondary"><i class="fas fa-redo mr-2"></i>Làm lại</button>
+        </div>
+        <div class="card card-primary card-outline text-sm">
+            <div class="card-header">
+                <h3 class="card-title">Chi tiết <?=$config['static'][$type]['title_main']?></h3>
+            </div>
+            <div class="card-body">
+                <?php if($config['static'][$type]['images']) { ?>
+                    <div class="form-group">
+                        <label class="change-photo" for="file">
+                            <p>Upload hình ảnh:</p>
+                            <div class="rounded">
+                                <img class="rounded img-upload" src="<?=UPLOAD_NEWS.$item['photo']?>" onerror="src='assets/images/noimage.png'" alt="Alt Photo"/>
+                                <strong>
+                                    <b class="text-sm text-split"></b>
+                                    <span class="btn btn-sm bg-gradient-success"><i class="fas fa-camera mr-2"></i>Chọn hình</span>
+                                </strong>
+                            </div>
+                        </label>
+                        <strong class="d-block mt-2 mb-2 text-sm"><?php echo "Width: ".$config['static'][$type]['width']." px - Height: ".$config['static'][$type]['height']." px (".$config['static'][$type]['img_type'].")" ?></strong>
+                        <div class="custom-file my-custom-file d-none">
+                            <input type="file" class="custom-file-input" name="file" id="file">
+                            <label class="custom-file-label" for="file">Chọn file</label>
+                        </div>
+                    </div>
+                <?php } ?>
+                <?php if($config['static'][$type]['file']) { ?>
+                    <div class="form-group">
+                        <label class="change-file mb-1 mr-2" for="file-taptin">
+                            <p>Upload tập tin:</p>
+                            <strong class="ml-2">
+                                <span class="btn btn-sm bg-gradient-success"><i class="fas fa-file-upload mr-2"></i>Chọn tập tin</span>
+                                <div><b class="text-sm text-split"></b></div>
+                            </strong>
+                        </label>
+                        <strong class="d-block mt-2 mb-2 text-sm"><?php echo $config['static'][$type]['file_type']; ?></strong>
+                        <div class="custom-file my-custom-file d-none">
+                            <input type="file" class="custom-file-input" name="file-taptin" id="file-taptin">
+                            <label class="custom-file-label" for="file-taptin">Chọn file</label>
+                        </div>
+                        <?php if($item['taptin']) { ?>
+                            <a class="btn btn-sm bg-gradient-primary text-white d-inline-block align-middle p-2 rounded mb-1" href="<?=UPLOAD_FILE.$item['taptin']?>" title="Download tập tin hiện tại"><i class="fas fa-download mr-2"></i>Download tập tin hiện tại</a>
+                        <?php } ?>
+                    </div>
+                <?php } ?>
+                <?php if($config['static'][$type]['link']) { ?>
+                    <div class="form-group">
+                        <label for="link">Link:</label>
+                        <input type="text" class="form-control" name="data[link]" id="link" placeholder="Link" value="<?=$item['link']?>">
+                    </div>
+                <?php } ?>
+                <?php if($config['static'][$type]['video']) { ?>
+                    <div class="form-group">
+                        <label for="link_video">Video:</label>
+                        <input type="text" class="form-control" name="data[link_video]" id="link_video" onchange="youtubePreview(this.value,'#loadVideo');" placeholder="Video" value="<?=$item['link_video']?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="link_video">Video preview:</label>
+                        <div><iframe id="loadVideo" width="250" src="//www.youtube.com/embed/<?=$func->getYoutube($item['link_video'])?>" <?=($item["link_video"]=="")?"height='0'":"height='150'";?> frameborder="0" allowfullscreen></iframe></div>
+                    </div>
+                <?php } ?>
+                <div class="form-group">
+                    <label for="hienthi" class="d-inline-block align-middle mb-0 mr-2">Hiển thị:</label>
+                    <div class="custom-control custom-checkbox d-inline-block align-middle">
+                        <input type="checkbox" class="custom-control-input hienthi-checkbox" name="data[hienthi]" id="hienthi-checkbox" <?=(!isset($item['hienthi']) || $item['hienthi']==1)?'checked':''?>>
+                        <label for="hienthi-checkbox" class="custom-control-label"></label>
+                    </div>
+                </div>
+                <?php if($config['static'][$type]['tieude'] || $config['static'][$type]['mota'] || $config['static'][$type]['noidung']) { ?>
+                    <div class="card card-primary card-outline card-outline-tabs">
+                        <div class="card-header p-0 border-bottom-0">
+                            <ul class="nav nav-tabs" id="custom-tabs-three-tab-lang" role="tablist">
+                                <?php foreach($config['website']['lang'] as $k => $v) { ?>
+                                    <li class="nav-item">
+                                        <a class="nav-link <?=($k=='vi')?'active':''?>" id="tabs-lang" data-toggle="pill" href="#tabs-lang-<?=$k?>" role="tab" aria-controls="tabs-lang-<?=$k?>" aria-selected="true"><?=$v?></a>
+                                    </li>
+                                <?php } ?>
+                            </ul>
+                        </div>
+                        <div class="card-body card-article">
+                            <div class="tab-content" id="custom-tabs-three-tabContent-lang">
+                                <?php foreach($config['website']['lang'] as $k => $v) { ?>
+                                    <div class="tab-pane fade show <?=($k=='vi')?'active':''?>" id="tabs-lang-<?=$k?>" role="tabpanel" aria-labelledby="tabs-lang">
+                                        <?php if($config['static'][$type]['tieude']) { ?>
+                                            <div class="form-group">
+                                                <label for="ten<?=$k?>">Tiêu đề (<?=$k?>):</label>
+                                                <input type="text" class="form-control for-seo" name="data[ten<?=$k?>]" id="ten<?=$k?>" placeholder="Tiêu đề (<?=$k?>)" value="<?=$item['ten'.$k]?>" <?=($k=='vi')?'required':''?>>
+                                            </div>
+                                        <?php } ?>
+                                        <?php if($config['static'][$type]['mota']) { ?>
+                                            <div class="form-group">
+                                                <label for="mota<?=$k?>">Mô tả (<?=$k?>):</label>
+                                                <textarea class="form-control for-seo <?=($config['static'][$type]['mota_cke'])?'form-control-ckeditor':''?>" name="data[mota<?=$k?>]" id="mota<?=$k?>" rows="5" placeholder="Mô tả (<?=$k?>)"><?=htmlspecialchars_decode($item['mota'.$k])?></textarea>
+                                            </div>
+                                        <?php } ?>
+                                        <?php if($config['static'][$type]['noidung']) { ?>
+                                            <div class="form-group">
+                                                <label for="noidung<?=$k?>">Nội dung (<?=$k?>):</label>
+                                                <textarea class="form-control for-seo <?=($config['static'][$type]['noidung_cke'])?'form-control-ckeditor':''?>" name="data[noidung<?=$k?>]" id="noidung<?=$k?>" rows="5" placeholder="Nội dung (<?=$k?>)"><?=htmlspecialchars_decode($item['noidung'.$k])?></textarea>
+                                            </div>
+                                        <?php } ?>
+                                    </div>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+        <?php if($config['static'][$type]['seo']) { ?>
+            <div class="card card-primary card-outline text-sm">
+                <div class="card-header">
+                    <h3 class="card-title">Nội dung SEO</h3>
+                    <a class="btn btn-sm bg-gradient-success d-inline-block text-white float-right create-seo" title="Tạo SEO">Tạo SEO</a>
+                </div>
+                <div class="card-body">
+                    <?php
+                        $seoDB = $seo->getSeoDB(0,$com,'capnhat',$type);
+                        include TEMPLATE.LAYOUT."seo.php";
+                    ?>
+                </div>
+            </div>
+        <?php } ?>
+        <div class="card-footer text-sm">
+            <button type="submit" class="btn btn-sm bg-gradient-primary submit-check"><i class="far fa-save mr-2"></i>Lưu</button>
+            <button type="reset" class="btn btn-sm bg-gradient-secondary"><i class="fas fa-redo mr-2"></i>Làm lại</button>
+        </div>
+    </form>
+</section>
---
+++ ajax/ajax_delete_cart.php
@@ -0,0 +1,17 @@
+<?php
+	include "ajax_config.php";
+
+	$code = htmlspecialchars($_POST['code']);
+	$ship = htmlspecialchars($_POST['ship']);
+	$endow = htmlspecialchars($_POST['endow']);
+	$cart->remove_product($code);
+	
+	$max = count($_SESSION['cart']);
+	$temp = $cart->get_order_total();
+	$tempText = number_format($temp,0, ',', '.')."đ";
+	$total = $cart->get_order_total() + $ship - $endow;
+	$totalText = number_format($total,0, ',', '.')."đ";
+
+	$data = array('max' => $max, 'temp' => $temp, 'tempText' => $tempText, 'total' => $total, 'totalText' => $totalText);
+	echo json_encode($data);
+?>
